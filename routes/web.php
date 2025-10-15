cfcfb71a209e154bf1520b5f54707fea72592897
azizah24si <azizah24si@mahasiswa.pcr.ac.id>
2025-10-15 11:15:29 +0700

    pertemuan6

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
 
 
 Route::get('/', function () {
@@ -32,3 +33,8 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
+
+Route::get('dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
