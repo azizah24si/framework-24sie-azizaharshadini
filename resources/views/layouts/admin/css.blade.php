bd8ed8de563d4c3571adbe84e5d55c7ce7969a2a
azizah24si <azizah24si@mahasiswa.pcr.ac.id>
2025-10-22 10:34:17 +0700

    pertemuan7

---
+++ resources/views/layouts/admin/css.blade.php
@@ -0,0 +1,4 @@
+{{-- starts css --}}
+    <!-- Volt CSS -->
+    <link type="text/css" href="{{ asset('assets-admin/css/volt.css') }}" rel="stylesheet">
+    {{-- end CSS --}}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\PelangganController;
@@ -38,3 +39,6 @@ Route::get('dashboard', [DashboardController::class, 'index'])
     ->name('dashboard');
 
 Route::resource('pelanggan', PelangganController::class);
+
+Route::resource('user', UserController::class);
+
