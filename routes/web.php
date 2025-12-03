17108c169fc1df7df7693f4b9f81878b4d4fb008
azizah24si <azizah24si@mahasiswa.pcr.ac.id>
2025-12-03 10:20:25 +0700

    pertemuan13

--- routes/web.php
+++ routes/web.php
@@ -49,5 +49,6 @@ Route::resource('user', UserController::class);
 
 Route::get('auth', [AuthController::class, 'index'])->name('auth');
 Route::post('auth/login', [AuthController::class, 'login'])->name('auth.login');
+Route::get('auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
 
